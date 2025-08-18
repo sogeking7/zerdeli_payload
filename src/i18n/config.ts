f61dde039e34052f08e2e8acaa5f8ca6995ad17b
Kaiyrkhan <kairolla222@gmail.com>
2025-08-18 22:21:52 +0500

    set default lang kk

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,4 +1,4 @@
 export type Locale = (typeof locales)[number]
 
 export const locales = ['en', 'ru', 'kk'] as const
-export const defaultLocale: Locale = 'ru'
+export const defaultLocale: Locale = 'kk'
